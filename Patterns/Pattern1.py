2319657a4cb2ba27bc6e955fd6f8245ad96def27
Pooja Khoday <poojakhoday06@gmail.com>
2026-04-02 09:33:18 +0000

    Added Pattern1.py(square pattern)

---
+++ Patterns/Pattern1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def pattern1(self,n):
+        for i in range(n):
+            for j in range(n):
+                print("*" , end = "")
+            print()
+n = int(input())            
+obj = Solution()
+obj.pattern1(n)                
+
