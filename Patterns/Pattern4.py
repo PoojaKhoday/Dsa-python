dd16c90fab553096adfce22a09cc1e5fd3536206
Pooja Khoday <poojakhoday06@gmail.com>
2026-04-03 16:33:41 +0000

    Added Pattern2.py

--- Patterns/Pattern4.py
+++ Patterns/Pattern4.py
@@ -1,10 +1,9 @@
 class Solution:
-    def pattern2(self, n):
+    def Pattern3(self, n):
         for i in range(n):
             for j in range(i+1):
-                print("*", end="")
-            print() 
-#n = 7  
+                print(i+1, end = " ")
+            print()
 n = int(input())
 obj = Solution()
-obj.pattern2(n)
+obj.Pattern3(n)
