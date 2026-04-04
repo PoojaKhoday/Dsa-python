acac6d4e20f081a61e8d70f290ea7194c85aac32
Pooja Khoday <poojakhoday06@gmail.com>
2026-04-04 16:53:08 +0000

    Added Pattern7.py

---
+++ Patterns/Pattern7.py
@@ -0,0 +1,13 @@
+class Solution:
+    def Pattern5(self, n):
+        for i in range(n):
+            for j in range(n-i):
+                print(" ",end = " ")
+            for j in range(2*i+1):
+                print("*",end = " ") 
+            for j in range(n-i):
+                print(" ",end = " ")    
+            print()
+n = int(input())
+obj = Solution()
+obj.Pattern5(n)
