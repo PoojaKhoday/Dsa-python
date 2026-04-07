cc4d95c06238cbca7f169b14340d9d616f822e3d
Pooja Khoday <poojakhoday06@gmail.com>
2026-04-07 11:08:50 +0000

    Added Pattern12.py

--- Pattern12.py
+++ Pattern12.py
@@ -1,5 +1,5 @@
 class Solution:
-    def Pattern11(self,n):
+    def Pattern12(self,n):
         for i in range(1,n+1):
             #num
             for j in range(1, i+1):
@@ -7,13 +7,12 @@ class Solution:
             #space
             for j in range(4 * (n - i)):
                 print(" ", end = "")
-                j = -2
             #num
             for j in range(i,0,-1):
                 print(j, end = " ")
             print()
 n = int(input()) 
 obj = Solution()
-obj.Pattern11(n)                   
+obj.Pattern12(n)                   
 
                     
