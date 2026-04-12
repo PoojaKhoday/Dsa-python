eb279a72e719cddc2ac536208db4c0b8e99c9afa
Pooja Khoday <poojakhoday06@gmail.com>
2026-04-12 17:06:16 +0000

    Added IsPrime.py

---
+++ BasicMaths/IsPrime.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPrime(self, n):
+        if n <= 1:
+            return False
+        
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                return False
+        
+        return True
+n = int(input()) 
+obj = Solution() 
+print(obj.isPrime(n))        
