92b2c41f8cfce1cb66dd1790618620f69015d00c
Pooja Khoday <poojakhoday06@gmail.com>
2026-04-18 18:00:35 +0000

    Added dry run to PalindromeUsingRecursion.py

--- Recursion/PalindromeUsingRecursion.py
+++ Recursion/PalindromeUsingRecursion.py
@@ -11,4 +11,45 @@ class Solution:
 s = input().strip()
 obj=Solution()
 print(obj.palindromeCheck(s))
-     
+
+# Input:
+# s = "madam"
+
+# n = 5
+
+# Call:
+# helper(0)
+
+# ---------------------------------
+
+# helper(0):
+# i = 0
+# s[0] == s[4] → m == m → True
+# Call helper(1)
+
+# ---------------------------------
+
+# helper(1):
+# i = 1
+# s[1] == s[3] → a == a → True
+# Call helper(2)
+
+# ---------------------------------
+
+# helper(2):
+# i = 2
+# i >= n//2 → 2 >= 2 → True
+# Return True
+
+# ---------------------------------
+
+# Backtracking:
+
+# helper(2) → True
+# helper(1) → True
+# helper(0) → True
+
+# ---------------------------------
+
+# Final Output:
+# True     
