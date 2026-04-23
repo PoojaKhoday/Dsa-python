fa013f87be9afcae0a384bcaeb8527b4425eec7f
Pooja Khoday <poojakhoday06@gmail.com>
2026-04-23 16:52:45 +0000

    Added CountSumK.py

---
+++ Recursion/CountSumK.py
@@ -0,0 +1,22 @@
+class Solution:
+    def SumOfK(self, i, s, arr, target, n):
+        if i == n:
+            if s == target:
+                return 1
+            return 0
+
+        # Include current element
+        take = self.SumOfK(i + 1, s + arr[i], arr, target, n)
+
+        # Exclude current element
+        not_take = self.SumOfK(i + 1, s, arr, target, n)
+
+        return take + not_take
+
+
+n = int(input())
+arr = list(map(int, input().split()))
+target = int(input())
+
+obj = Solution()
+print(obj.SumOfK(0, 0, arr, target, n))
