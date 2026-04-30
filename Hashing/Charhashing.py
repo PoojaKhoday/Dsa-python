6df405629de89d387b9b5b6586327d4ba86bbdb9
Pooja Khoday <poojakhoday06@gmail.com>
2026-04-30 17:16:19 +0000

    Added charhashing.py

---
+++ Hashing/Charhashing.py
@@ -0,0 +1,28 @@
+def character_hashing():
+    s = input("Enter string: ")
+
+    freq = {}
+
+    # character hashing
+    for ch in s:
+        freq[ch] = freq.get(ch, 0) + 1
+
+    print("Enter characters to query (0 to stop):")
+
+    while True:
+        ch = input()
+
+        if ch == "0":
+            break
+
+        print(freq.get(ch, 0))
+
+character_hashing()
+
+#sample input and output
+#Enter string: banana\
+#Enter characters to query (0 to stop):
+#a
+#3
+#n
+#2
