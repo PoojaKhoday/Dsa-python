f0ce3ecba7d9e6697b4dc6dcb8ecd6a92fe60bc1
Pooja Khoday <poojakhoday06@gmail.com>
2026-05-01 16:59:29 +0000

    Solved Twosum.py

---
+++ Hashing/Leetcode/TwoSum.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1 , len(nums)):
+                if nums[i] + nums[j] == target:
+                    return[i, j]
+nums = list(map(int, input().strip('[]').split(',')))
+target = int(input())
+obj = Solution()
+print(obj.twoSum(nums,target))
