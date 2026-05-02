b36c717b9bf988967a6720f753feb5ca0d133a63
Pooja Khoday <poojakhoday06@gmail.com>
2026-05-02 17:29:21 +0000

    Added leetcode easy problem solution>palindrome check.

---
+++ Leetcode/Easy/Palindrome.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        a = str(x)
+        if a == a[::-1]:
+            return True
+        return False     
+x = int(input())
+obj = Solution()
+print(obj.isPalindrome(x))            
